b876da13071f61c21f040eb2423ef7ccf99dcbb6
Marlon Mapile <Marlonmapile@gmail.com>
2017-01-13 00:19:23 -0800

    Finished with lecture 3

--- colorGame.js
+++ colorGame.js
@@ -4,13 +4,14 @@ var colors = [
 	"rgb(0, 255, 0)",
 	"rgb(0, 255, 255)",
 	"rgb(0, 0, 255)",
-	"rgb(255, 0, 0)"
+	"rgb(255, 0, 255)"
 ]
 
-var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
-var colorDisplay = document.getElementById("colorDisplay");
 
+var squares = document.querySelectorAll(".square");
+var pickedColor = pickColor(); // colors[3]
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 colorDisplay.textContent = pickedColor;
 
 for (var i = 0; i < squares.length; i++) {
@@ -18,10 +19,23 @@ for (var i = 0; i < squares.length; i++) {
 	squares[i].addEventListener("click", function(){
 		var clickedColor = this.style.background;
 		if (clickedColor === pickedColor) {
-			alert("right");
+			messageDisplay.textContent = "CORRECT";
+			changeColor(clickedColor);
 		} else {
-			alert("wrong");
+			this.style.background = "#232323";
+			messageDisplay.textContent = "TRY AGAIN!";
 		}
 	});
 }
 
+
+function changeColor(color) {
+	for (var i = 0; i < squares.length; i++) {
+		squares[i].style.background = color;
+	}
+}
+
+function pickColor() {
+	var randomNum = Math.floor(Math.random() * colors.length);
+	return colors[randomNum]; // colors is an array
+}
